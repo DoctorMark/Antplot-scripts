4172ed16135bace8ab94f9d0d04d5c8a27edb3c0
Mark Agate <mark@agate.me.uk>
2022-06-08 13:47:19 +0100

    Update test.m

--- test.m
+++ test.m
@@ -50,6 +50,8 @@ for n = 1:4                 % do 4 lots of the recurrence pattern
 
     board(x, y) = !p * 63
 
+    bird(x, y) = !p * 63;
+    
     % Move the ant to the next pixel
     x += dir_x;
     y += dir_y;
